97269cb638ec9a3c45cff1cc339428e1dd1ece43
Alex Romauld <alex.romauld@gmail.com>
2023-10-02 18:59:14 -0400

    add melpa

--- init.el
+++ init.el
@@ -27,6 +27,13 @@
 (set-frame-font "Cascadia Mono 10" nil t)
 
 
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+;; Comment/uncomment this line to enable MELPA Stable if desired.  See `package-archive-priorities`
+;; and `package-pinned-packages`. Most users will not need or want to do this.
+;;(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+(package-initialize)
+
 
 (require 'redo+)
 (global-set-key (kbd "C-/") 'undo) ;(global-set-key [(control z)] 'undo)
