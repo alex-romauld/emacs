9420e1e8d903efd74b556bb07ddf349bf3aca03a
Alex Romauld <alex.romauld@gmail.com>
2023-11-08 22:19:11 -0500

    add drag-stuff

--- init.el
+++ init.el
@@ -89,39 +89,6 @@
     (setq p2 (point))
     (delete-region p1 p2)))
 
-;; Moves Line/Region Up/Down
-
-(defun move-text-internal (arg)
-  (cond
-   ((and mark-active transient-mark-mode)
-    (if (> (point) (mark))
-        (exchange-point-and-mark))
-    (let ((column (current-column))
-          (text (delete-and-extract-region (point) (mark))))
-      (forward-line arg)
-      (move-to-column column t)
-      (set-mark (point))
-      (insert text)
-      (exchange-point-and-mark)
-      (setq deactivate-mark nil)))
-   (t
-    (beginning-of-line)
-    (when (or (> arg 0) (not (bobp)))
-      (forward-line)
-      (when (or (< arg 0) (not (eobp)))
-        (transpose-lines arg))
-      (forward-line -1)))))
-
-(defun move-text-down (arg)
-  "Move region (transient-mark-mode active) or current line arg lines down."
-  (interactive "*p")
-  (move-text-internal arg))
-
-(defun move-text-up (arg)
-  "Move region (transient-mark-mode active) or current line arg lines up."
-  (interactive "*p")
-  (move-text-internal (- arg)))
-
 ;; Compilation/Running
 
 (defun find-project-directory-recursive (file)
@@ -419,12 +386,13 @@
 (global-set-key (kbd "M-<backspace>") 'my-backward-delete-word)
 (global-set-key (kbd "M-R") 'revert-buffer)
 
-(global-set-key [M-up]   'move-text-up)
-(global-set-key [M-down] 'move-text-down)
-
 (require 'redo+)
 (global-set-key (kbd "C-/") 'undo)
 (global-set-key (kbd "C-?") 'redo)
+
+(require 'drag-stuff)
+(drag-stuff-global-mode t)
+(drag-stuff-define-keys)
 
 ;; Navigation
 (define-key gkeymap (kbd "C-<tab>")   'switch-to-next-buffer)
